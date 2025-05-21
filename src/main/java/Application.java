4df87ae57910c0a1bf2dc9a704ef1cadb96ed911
nico mueller <hans@hans.de>
2025-05-21 15:54:17 +0200

    ControlModule Scanner

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,8 +1,18 @@
+import lombok.Builder;
+import share.Baggage;
+import share.CheckinDesk;
+import share.Destination;
+
+import java.security.Provider;
+
+@Builder
 public class Application {
     public static void main(String[] args) {
 
+        Baggage baggage = new Baggage();
 
-
+        CheckinDesk checkinDesk = new CheckinDesk();
+        checkinDesk.checkInBaggage(baggage, Destination.BER);
 
     }
 }
--- src/main/java/share/CheckinDesk.java
+++ src/main/java/share/CheckinDesk.java
@@ -1,7 +1,17 @@
 package share;
 
-public class CheckinDesk {
 
+import Interfaces.IBaggageTagPrinter;
+import Interfaces.IScanner;
+
+public class CheckinDesk {
+    private IScanner scanner = Scanner.builder().location("CheckinDesk").prewiousLocation("").build();
+    private IBaggageTagPrinter baggageTagPrinter = new BaggageTagPrinter();
+
+        public void checkInBaggage(Baggage baggage, Destination destination) {
+            baggageTagPrinter.prtintTag(destination);
+
+        }
 
 
 }
